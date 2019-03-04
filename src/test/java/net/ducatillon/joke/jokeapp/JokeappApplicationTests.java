3ad5742b97bacda13679aa1e7968bc2a9de6a829
Francois DUCATILLON <francois.ducatillon@decathlon.com>
2019-03-04 13:40:08 +0100

    first commit

---
+++ src/test/java/net/ducatillon/joke/jokeapp/JokeappApplicationTests.java
@@ -0,0 +1,13 @@
+package net.ducatillon.joke.jokeapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class) @SpringBootTest public class JokeappApplicationTests {
+
+	@Test public void contextLoads() {
+	}
+
+}
